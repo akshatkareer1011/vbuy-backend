c217d49a1ffdf94675a5928fa586a0e9c792cc58
akshatkareer1011 <49092873+akshatkareer1011@users.noreply.github.com>
2025-03-26 22:07:14 +0530

    Update server.js

--- src/server.js
+++ src/server.js
@@ -55,4 +55,4 @@ app.get("/order", (req, res) => {
 });
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Server up and running on port ${PORT}!`));
+app.listen(PORT, '0.0.0.0' () => console.log(`Server up and running on port ${PORT}!`));
